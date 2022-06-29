18f04f75cadb110eab9f6e4f5ddc849b52a3ec92
EamonYin <xiaominglovejava@163.com>
2022-06-29 13:27:31 +0800

    Eamon提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
-public class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("这里是 main 分支");
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("这里是 main 分支 手动制造冲突");
     }
 }
